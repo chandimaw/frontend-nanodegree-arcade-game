7a7111a733b4cac204ec65565a5a361f8ee55278
chandimaw <chandimawijendra@gmail.com>
2017-04-02 22:00:40 +0800

    Updated app.js, engine.js and README

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-frontend-nanodegree-arcade-game
+# Front-end Nanodegree Clasic Arcade Game Clone
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+### Summary
 
+Frogger Arcade is a variation of the classic Frogger arcade game.
+
+### Frogger Game Description:
+
+In this game you have a Player and Enemies (Bugs). The goal is simple, get the hero character to move across the gray bricks towards the water without getting hit by the enemy bugs.
+
+The enemies move in varying speeds on the gray bricks. Once the player collides with an enemy, the game is reset and the player moves back to the start square. Once the player reaches the water the game is won.
+
+### To play the game:
+
+- Use arrows to move your hero character by using up/down/right/left on your keyboard
+- Avoid the enemies dashing from left to right over the gray bricks
+- The grass tiles are safe
+
+
+
+
+### Project Requirements:
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission.
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+### License
+
+Arcade Game is released under the [MIT License](http://choosealicense.com/licenses/mit/).
--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+
+var enemySpeed = 200;
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x,y,speed) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = x;
+	  this.y = y;
+
+    this.speed = Math.floor(200 + (Math.random() * enemySpeed));
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.speed * dt);
+
+    if (this.x > 550){
+      this.x = -100;
+      this.speed = Math.floor(200 + (Math.random() * enemySpeed));
+      if (this.y > 226){
+        this.y = 60;
+      }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +39,77 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+//player class -- constructor
+var Player = function (x,y) {
+
+    this.x = 202;
+    this.y = 404;
+
+    this.sprite = 'images/char-boy.png';
+}
+
+//reset after player hits the water
+Player.prototype.update = function() {
+
+  if (this.y < 10)
+  {
+    alert('Congratulations!');
+    player.reset();
+  }
+};
+
+Player.prototype.render = function() {
+	ctx.drawImage (Resources.get(this.sprite), this.x, this.y);
+}
+
+//ensure that the player is within the board
+Player.prototype.handleInput = function(direction) {
+	if(direction == 'left' && this.x > 0) {
+		this.x -= 100;
+	}
+	if(direction == 'right' && this.x < 350) {
+		this.x += 100;
+	}
+	if(direction == 'up' && this.y > 50) {
+		this.y -= 100;
+	}
+	if(direction == 'down' && this.y < 400) {
+		this.y += 100;
+	}
+}
+
+
+//Reset function to bring player back to starting Y coordinate
+Player.prototype.reset = function() {
+
+  this.x = 202;
+  this.y = 404;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy (200, 60);
+var enemy2 = new Enemy (0, 143);
+var enemy3 = new Enemy (50, 226);
+var enemy4 = new Enemy (-300, 226);
 
+var allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
+var player = new Player();
+
+//Collision function to reset player back to starting Y coordinate
+var checkCollisions = function() {
+
+  allEnemies.forEach(function(enemy) {
+
+  if(enemy.x < player.x + 30 && enemy.x + 60 > player.x && enemy.y < player.y + 60 && enemy.y + 40 > player.y)
+    {
+        alert('Too bad, try again!');
+		    player.reset();
+	     }
+	});
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
